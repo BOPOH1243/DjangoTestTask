117c8198f25534d2bd9569b171bde5fef84272b5
BOPOH1243 <tenpenni@mail.ru>
2024-10-06 14:15:12 +0500

    довёл всё до ума, остается создать нужные страницы и закинуть на гитхаб

--- DjangoTestTask/DjangoTest/templatetags/menu_tags.py
+++ DjangoTestTask/DjangoTest/templatetags/menu_tags.py
@@ -4,11 +4,23 @@ from DjangoTest.models import MenuItem
 
 register = template.Library()
 
+def get_active_menu(menu_items, current_url):
+    """ Рекурсивная функция для нахождения активного пункта меню и всех его родителей """
+    for item in menu_items:
+        if item.get_absolute_url() == current_url:
+            return [item]
+        children = get_active_menu(item.children.all(), current_url)
+        if children:
+            return [item] + children
+    return []
+
 @register.inclusion_tag('menu.html', takes_context=True)
 def draw_menu(context, menu_name):
     current_url = context['request'].path
     menu_items = MenuItem.objects.filter(parent__isnull=True)
+    active_items = get_active_menu(menu_items, current_url)
     return {
         'menu_items': menu_items,
+        'active_items': active_items,
         'current_url': current_url
     }
